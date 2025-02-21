9db3603dec8209930eb729805c20313e3c371dd2
avina7061 <avinashchy73@gmail.com>
2025-02-22 01:32:41 +0530

    update

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -33,7 +33,7 @@ const ChatPage = () => {
   const sendMessageHandler = async (receiverId) => {
     try {
       const res = await axios.post(
-        `http://localhost:8003/api/v1/message/send/${receiverId}`,
+        `https://educationmedia.onrender.com/api/v1/message/send/${receiverId}`,
         { textMessage },
         {
           headers: {
